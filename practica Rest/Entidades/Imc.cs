473e414e8af6eed180bdd55f113d4571f3582f71
LeaRamos <47989263+LeaRamos@users.noreply.github.com>
2022-04-12 18:42:22 -0300

    Add files via upload

---
+++ practica Rest/Entidades/Imc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Imc
+    {
+        public double peso { get; set; }
+        public double altura { get; set; }
+    }
+}
